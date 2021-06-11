0af69c670f4ea8a8ad3d18172c6d1dddbc1384fa
Guilherme S. G. Cordeiro <guisg.cordeiro@gmail.com>
2021-06-10 21:38:27 -0300

    Código-fonte revisado e aprimorado
    * Redução e melhoramento do código-fonte, de 2304 linhas para 750;
    * Tela de apresentação adicionada;
    * Um efeito das linhas aparecendo na tela foi adicionado.

--- Braile-download/Braile.c
+++ Braile-download/Braile.c
@@ -1,45 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include <windows.h>
 #include <locale.h>
 #include <math.h>
+#include <string.h>
 
 int main(void){
     int a=0;
     setlocale(LC_ALL,"Portuguese");
-        do{
-            a = cabecalho();
-            if((a > 7) || (a < 1)){
+    apresentacao();
+    do{
+        a = menu();
+        switch(a){
+            case 1:
+                opcao1();
+                break;
+            case 2:
+                opcao2();
+                break;
+            case 3:
+                opcao3();
+                break;
+            case 4:
+                opcao4();
+                break;
+            case 5:
+                opcao5();
+                break;
+            case 6:
+                opcao6();
+                break;
+            case 7:
+                continue;
+            default:
                 system("cls");
                 printf("\n Opção inválida! Por favor, digite um número válido entre '1' e '7'...");
                 Sleep(1200);
                 system("cls");
-            }else{
-                switch(a){
-                    case 1:
-                        opcao1();
-                        break;
-                    case 2:
-                        opcao2();
-                        break;
-                    case 3:
-                        opcao3();
-                        break;
-                    case 4:
-                        opcao4();
-                        break;
-                    case 5:
-                        opcao5();
-                        break;
-                    case 6:
-                        opcao6();
-                        break;
-                };
-            system("cls");
-            }
-        }while(a != 7);
+        };
         system("cls");
-        github();
-        printf("\n   **** Pressione qualquer tecla para sair do jogo ****");
-        getche();
+    }while(a != 7);
+    system("cls");
+    github();
+    printf("\n        **** Pressione qualquer tecla para sair do jogo ****");
+    getch();
 }
